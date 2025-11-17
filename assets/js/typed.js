4bfa3167f3700ac668dc4043ee339d4ef450b1d7
RobyRF <103549681+ObiKusakabe@users.noreply.github.com>
2025-11-17 21:10:46 +0700

    Add files via upload

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -10,3 +10,15 @@ var typed = new Typed("#typed", {
   backDelay: 1300,
   loop: true,
 });
+var typed = new Typed("#typed2", {
+  strings: [
+    // "Saya Roby -",
+    "Belajar",
+    "Berkembang",
+    "Berbagi"
+  ],
+  typeSpeed: 100,
+  backSpeed: 70,
+  backDelay: 1800,
+  loop: true,
+});
